e27eb089a3ed69ce4264827b33c0ddc0700dcb63
Ryan D'souza <dsouzarc@gmail.com>
2014-11-19 17:17:58 -0500

    Fixed custom linked list to work. Added main testing method

--- CustomLinkedList.java
+++ CustomLinkedList.java
@@ -13,23 +13,49 @@ public class CustomLinkedList {
     }
 
     public void add(final Object value) { 
-        final Node nodeVal = new Node(value);
-        
-        //Node to get to last node
-        Node toEnd = this.head;
 
-        while(toEnd.next != null) { 
-            toEnd = toEnd.next;
+        if(head.value == null) { 
+            head.value = value;
         }
 
-        //Add newest node to end of list
-        toEnd.next = nodeVal;
+        else { 
+            final Node nodeVal = new Node(value);
+        
+            //Node to get to last node
+            Node toEnd = this.head;
+
+            while(toEnd.next != null) { 
+                toEnd = toEnd.next;
+            }
+
+            //Add newest node to end of list
+            toEnd.next = nodeVal;
+        }
         size++;
     }
 
-    /** More methods will be implemented when the time comes */
+    /** More methods will be implemented when the need comes */
 
     public int size() { 
         return this.size;
+    }
+
+    public Node getHead() { 
+        return this.head;
+    }
+
+    public static void main(String[] ryan) { 
+        final CustomLinkedList myList = new CustomLinkedList();
+
+        for(String s : ryan) { 
+            myList.add(s);
+        }
+
+        Node head = myList.getHead();
+
+        while(head != null) {
+            System.out.println("Stored Value\t" + head.value);
+            head = head.next;
+        }
     }
 }
