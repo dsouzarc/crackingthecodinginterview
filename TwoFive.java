0f8fc236a5edea8c0749cfb0aba4cf4fa9f13e09
Ryan D'souza <dsouzarc@gmail.com>
2014-12-03 22:09:45 -0500

    2.5

---
+++ TwoFive.java
@@ -0,0 +1,10 @@
+/** 
+ * Written by Ryan D'souza
+ * Cracking the Coding Interview 2.5
+ * Two numbers are stored in a linkedlist in reverse order
+ * so that ones digit is at head of list.
+ * Add two #s and return sum as linkedlist
+ * ex. (7 --> 1 --> 6) + (5 -> 9 -> 2) = 2 -> 1 -> 9 because 617 + 295 = 912 */
+
+public class TwoFive { 
+}
