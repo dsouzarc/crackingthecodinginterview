fd4e9d42a2fe3ba353ea95fc33be6c0a54512db4
Ryan D'souza <dsouzarc@gmail.com>
2014-12-15 08:43:38 -0500

    Added comments and run instructions

--- ThreeFive.java
+++ ThreeFive.java
@@ -1,7 +1,19 @@
 /** 
  * Written by Ryan D'souza
  * 3.5 Cracking the Coding Interview
- * A queue created by using two stacks */
+ * A queue created by using two stacks 
+ *
+ * Run Instructions: 
+ * 1. javac ThreeFive.java CustomStack.java Node.java
+ * 2. java ThreeFive name1 name2 name3 name4 name5
+ *
+ * OUTPUT:
+ * name1 
+ * name2
+ * name3
+ * name4
+ * name5
+ * */
 
 public class ThreeFive { 
     private final CustomStack oldestItems, newestItems;
@@ -11,28 +23,28 @@ public class ThreeFive {
         this.newestItems = new CustomStack();
     }
 
-    public int size() { 
-        return this.oldestItems.getSize() + this.newestItems.getSize();
-    }
-
     public void add(final Object toAdd) { 
-        //Add it to the newest stack
-        //newestItems.add(toAdd);
 
+        //Add all the items from the old stack to the new one, thereby reversing order
         while(this.oldestItems.getSize() != 0) {
             this.newestItems.add(this.oldestItems.pop());
         }
 
+        //Add the newest item
         this.newestItems.add(toAdd);
 
+        //Add items back to old stack, putting order in FIFO form
         while(this.newestItems.getSize() != 0) { 
             this.oldestItems.add(this.newestItems.pop());
         }
-
     }
 
     public Object pop() { 
         return this.oldestItems.pop();
+    }
+
+    public int size() { 
+        return this.oldestItems.getSize() + this.newestItems.getSize();
     }
 
     public static void main(String[] ryan) { 
