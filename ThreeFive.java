7d6652da9d2f3e2c6de72c084d5f66ff5185edd6
Ryan D'souza <dsouzarc@gmail.com>
2014-12-15 08:27:04 -0500

    Basics for 3.5

--- ThreeFive.java
+++ ThreeFive.java
@@ -4,4 +4,25 @@
  * A queue created by using two stacks */
 
 public class ThreeFive { 
+    private final CustomStack oldestItems, newestItems;
+
+    public ThreeFive() { 
+        this.oldestItems = new CustomStack();
+        this.newestItems = new CustomStack();
+    }
+
+    public int size() { 
+        return this.oldestItems.getSize() + this.newestItems.getSize();
+    }
+
+    public void add(final Object toAdd) { 
+    }
+
+    public static void main(String[] ryan) { 
+        final ThreeFive obj = new ThreeFive();
+
+        for(String r : ryan) { 
+            obj.add(r);
+        }
+    }
 }
