f970feb39f5e0446c8a0a0d2c2b4f54e47b552a3
Ryan D'souza <dsouzarc@gmail.com>
2014-12-14 22:12:54 -0500

    3.3

---
+++ ThreeThree.java
@@ -0,0 +1,45 @@
+/** 
+ * Written by Ryan D'souza
+ * 3.3 Cracking the Coding Interview
+ * A Stack of Stacks where a new stack is added after a certain # of elements have been added */
+
+public class ThreeThree { 
+    //Max size of a stack
+    private static final int MAX_SIZE = 5;
+
+    //A stack to hold all stacks
+    private final CustomStack masterStack;
+
+    //Current stack
+    private CustomStack stack;
+
+    public ThreeThree() { 
+        this.masterStack = new CustomStack();
+        this.stack = new CustomStack();
+
+        this.masterStack.add(this.stack);
+    }
+
+    public void add(final Object item) { 
+        if(this.stack.size == MAX_SIZE) { 
+            stack = new CustomStack();
+            stack.add(item);
+            masterStack.add(stack);
+        }
+
+        else { 
+            this.stack.add(item);
+        }
+    }
+
+    public Object pop() { 
+        if(this.stack.size == 0) { 
+            this.masterStack.pop();
+            this.stack = this.masterStack.peek();
+            return this.stack.pop();
+        }
+        else { 
+            return this.stack.pop();
+        }
+    }
+}
