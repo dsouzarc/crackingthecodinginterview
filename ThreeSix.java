c57524b2421ef3b03e6d3c831abb7149b5157b79
Ryan D'souza <dsouzarc@gmail.com>
2014-12-16 13:30:19 -0500

    3.6

---
+++ ThreeSix.java
@@ -0,0 +1,11 @@
+/** 
+ * Written by Ryan D'souza
+ * Cracking the Coding Interview 3.6
+ * Sorts a stack in ascending order
+ * Uses at most one additional stack to hold items
+ * No other data structures can be used */
+
+public class ThreeSix { 
+    private final CustomStack theStack;
+
+    public ThreeSix
