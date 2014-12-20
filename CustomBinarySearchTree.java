04c183c385c733d7eba8345e2701b0605155ff00
Ryan D'souza <dsouzarc@gmail.com>
2014-12-20 15:03:23 -0500

    Private class Node

--- CustomBinarySearchTree.java
+++ CustomBinarySearchTree.java
@@ -28,6 +28,21 @@ public class CustomBinarySearchTree {
             this(value, key);
             this.size = size + 1;
         }
+
+        public int getSize() { 
+            if(leftChild == null && rightChild == null) { 
+                return 0;
+            }
+            else if(leftChild == null) { 
+                return rightChild.getSize();
+            }
+            else if(rightChild == null) { 
+                return leftChild.getSize();
+            }
+            else {
+                return leftChild.getSize() + rightChild.getSize();
+            }
+        }
     }
 }
 
