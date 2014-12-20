56c1ec7dab0ee31a1a1f63db7b0f0c0d5a6c3510
Ryan D'souza <dsouzarc@gmail.com>
2014-12-20 15:45:36 -0500

    Node class

--- CustomBinarySearchTree.java
+++ CustomBinarySearchTree.java
@@ -3,4 +3,32 @@
  * Represents a binary search tree */
 
 public class CustomBinarySearchTree { 
+    private final Node root;
+
+    public static class Node { 
+        public Key key;
+        public Value value;
+        public Node leftChild, rightChild;
+        private int size;
+
+        public Node() { 
+            this.key = null;
+            this.value = null;
+            this.leftChild = null;
+            this.rightChild = null;
+            this.size = 0;
+        }
+
+        public Node(final Key key, final Value value) { 
+            this();
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(final Key key, final Value value, final int size) { 
+            this(key, value);
+            this.size = size;
+        }
+    }
+
 
